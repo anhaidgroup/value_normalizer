437e50f12fc44d9cbd10c5f7a917ded4d1d1ff52
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-06-22 21:30:48 +0000

    Added a domain to tackle CORS

--- normalizer/src/main/java/edu/wisc/entity/normalizer/services/ConfigurationService.java
+++ normalizer/src/main/java/edu/wisc/entity/normalizer/services/ConfigurationService.java
@@ -13,8 +13,8 @@ public class ConfigurationService {
     private static String WINDOWS_LOCATION="src/main/resources/csv/";
     private static String LINUX_LOCATION="/normalizer/csv/";
     public static String RESOURCE_LOCATION;
-    public static String CDRIVE_DOWNLOAD_URL="http://api.cdrive.columbusecosystem.com/download/?file_name=";
-    public static String CDRIVE_UPLOAD_URL="http://api.cdrive.columbusecosystem.com/upload/";
+    public static String CDRIVE_DOWNLOAD_URL="https://api.cdrive.columbusecosystem.com/download/?file_name=";
+    public static String CDRIVE_UPLOAD_URL="https://api.cdrive.columbusecosystem.com/upload/";
 
     /* Start Development Variables
     * The variables defined below are only used for development
@@ -22,8 +22,8 @@ public class ConfigurationService {
     */
     public static String CLIENT_ID = "3l8f6m6aCbrK25JYQqbISe4ftORcYK0y8jnkZQkH";
     public static String CLIENT_SECRET = "Uk0KdikMZQo1VpdW5mXVObAHX60Tet3xAvOWC5yOy3Le9v7aUaiiMPfRb96gW5JNlewExQOAAIx3rqcU3zLXfKGs1uR7lUYEccYjVgvTBBCtwVE15swH7oiE52QAcO6C";
-    public static String REDIRECT_URI = "http://a02aacfda951e11e9ba9712d8376863f-1653013854.us-east-1.elb.amazonaws.com/home/";
-    public static String AUTH_URL = "http://authentication.columbusecosystem.com/o/token/";
+    public static String REDIRECT_URI = "https://value-normalizer.columbusecosystem.com/home/";
+    public static String AUTH_URL = "https://authentication.columbusecosystem.com/o/token/";
     /* End Development Variables
     */
     public ConfigurationService() {
