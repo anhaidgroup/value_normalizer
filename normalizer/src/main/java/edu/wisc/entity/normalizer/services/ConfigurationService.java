2e05b9be12dbc3a9bf1eff5d5cf067f9028382a9
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-06-22 18:47:46 +0000

    Changed backend urls to use columbus domain

--- normalizer/src/main/java/edu/wisc/entity/normalizer/services/ConfigurationService.java
+++ normalizer/src/main/java/edu/wisc/entity/normalizer/services/ConfigurationService.java
@@ -13,8 +13,8 @@ public class ConfigurationService {
     private static String WINDOWS_LOCATION="src/main/resources/csv/";
     private static String LINUX_LOCATION="/normalizer/csv/";
     public static String RESOURCE_LOCATION;
-    public static String CDRIVE_DOWNLOAD_URL="http://acdb13cd77acb11e98ea412ac368fc7a-549133274.us-east-1.elb.amazonaws.com/api/v1/cdrive/download/?file_name=";
-    public static String CDRIVE_UPLOAD_URL="http://acdb13cd77acb11e98ea412ac368fc7a-549133274.us-east-1.elb.amazonaws.com/api/v1/cdrive/upload/";
+    public static String CDRIVE_DOWNLOAD_URL="http://api.cdrive.columbusecosystem.com/download/?file_name=";
+    public static String CDRIVE_UPLOAD_URL="http://api.cdrive.columbusecosystem.com/upload/";
 
     /* Start Development Variables
     * The variables defined below are only used for development
@@ -23,7 +23,7 @@ public class ConfigurationService {
     public static String CLIENT_ID = "3l8f6m6aCbrK25JYQqbISe4ftORcYK0y8jnkZQkH";
     public static String CLIENT_SECRET = "Uk0KdikMZQo1VpdW5mXVObAHX60Tet3xAvOWC5yOy3Le9v7aUaiiMPfRb96gW5JNlewExQOAAIx3rqcU3zLXfKGs1uR7lUYEccYjVgvTBBCtwVE15swH7oiE52QAcO6C";
     public static String REDIRECT_URI = "http://localhost:4200/value_normalizer";
-    public static String AUTH_URL = "http://ad09282b27aca11e98ea412ac368fc7a-1539065101.us-east-1.elb.amazonaws.com/authentication/o/token/";
+    public static String AUTH_URL = "http://authentication.columbusecosystem.com/o/token/";
     /* End Development Variables
     */
     public ConfigurationService() {
